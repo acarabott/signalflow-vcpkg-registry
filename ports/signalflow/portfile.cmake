44ee2ae57571aada9a2c50309ff0a79e6c8ffbe8
Arthur Carabott <arthur@arthurcarabott.com>
2022-08-26 17:26:41 -0700

    [signalflow] add SHA512

--- ports/signalflow/portfile.cmake
+++ ports/signalflow/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO acarabott/signalflow
   REF vcpkg 
-  SHA512 0
+  SHA512 59dbb938a87beec5f54778cff1500470f68565d83a53059908a2d0cf1452d0a1390e85d26447701ffefdc997b8118b78c7afc1947c6ab940eea0288b4784a42b
   HEAD_REF master
 )
 
