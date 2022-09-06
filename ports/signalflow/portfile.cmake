5ea5d63909a0935a72c643fda746413ead6ebd89
Arthur Carabott <arthur@arthurcarabott.com>
2022-09-06 00:21:58 -0700

    update signalflow portfile

--- ports/signalflow/portfile.cmake
+++ ports/signalflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO acarabott/signalflow
-  REF vcpkg 
-  SHA512 59dbb938a87beec5f54778cff1500470f68565d83a53059908a2d0cf1452d0a1390e85d26447701ffefdc997b8118b78c7afc1947c6ab940eea0288b4784a42b
+  REF 5b74073a5354c78b648285872284c3dd7dcbd24a
+  SHA512 877d0ddeddbefc4ec6df7380852987cbc7c7a1f3ef3378debd7dc136a39aad6ba3980168474bd495c4ef68d2bd711076746754183bd6724be7939b5f3ffaa5ed
   HEAD_REF master
 )
 
@@ -16,7 +16,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE.md"
