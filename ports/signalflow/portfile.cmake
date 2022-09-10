9c3d0220a321fa129029cd973926eea4b336e27d
Arthur Carabott <arthur@arthurcarabott.com>
2022-09-10 15:45:11 -0700

    working (local repo)

--- ports/signalflow/portfile.cmake
+++ ports/signalflow/portfile.cmake
@@ -1,8 +1,15 @@
-vcpkg_from_github(
+# vcpkg_from_github(
+#   OUT_SOURCE_PATH SOURCE_PATH
+#   REPO acarabott/signalflow
+#   REF 9c90ce0849e310841340ff7a09e250a6e8e553be
+#   SHA512 1c8c418bea7cc5df72e754f3001f7000cf7bbf1e71c361485aff6a7e44dded761fd9f091015012f747a232c168a43033fb4974204c340a5e358b046727b0a020
+#   HEAD_REF master
+# )
+
+vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO acarabott/signalflow
+  URL /Users/ac/src/signalflow
   REF 9c90ce0849e310841340ff7a09e250a6e8e553be
-  SHA512 1c8c418bea7cc5df72e754f3001f7000cf7bbf1e71c361485aff6a7e44dded761fd9f091015012f747a232c168a43033fb4974204c340a5e358b046727b0a020
   HEAD_REF master
 )
 
@@ -14,9 +21,8 @@ vcpkg_configure_cmake(
     -DCMAKE_BUILD_FILEIO=OFF
 )
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE.md"
