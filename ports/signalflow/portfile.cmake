9451d6d233f1d5e8a8c7ce80da2a68308272aba7
Arthur Carabott <arthur@arthurcarabott.com>
2022-10-09 21:50:53 -0700

    try installing include dir

--- ports/signalflow/portfile.cmake
+++ ports/signalflow/portfile.cmake
@@ -15,6 +15,8 @@ vcpkg_configure_cmake(
 )
 vcpkg_install_cmake()
 
+file(INSTALL "${SOURCE_PATH}/source/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(
